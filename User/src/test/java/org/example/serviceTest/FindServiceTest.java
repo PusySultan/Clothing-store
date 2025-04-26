94963da27f9f47f2bc303dd260fdeb3346f33bdb
DESKTOP-KLGCVKK\Kirill <kirillsoloduhin072@gmail.com>
2025-04-26 15:24:02 +0300

    added first test to finde service

--- User/src/test/java/org/example/serviceTest/FindServiceTest.java
+++ User/src/test/java/org/example/serviceTest/FindServiceTest.java
@@ -1,4 +1,71 @@
 package org.example.serviceTest;
 
-public class FindServiceTest {
+import org.example.model.Person;
+import org.example.repository.PersonRepository;
+import org.example.service.AuntService;
+import org.example.service.FindService;
+import org.example.service.RegService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class FindServiceTest
+{
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private FindService findService;
+
+    private Person testPerson;
+
+    @BeforeEach
+    void setUp() {
+        // Устанавливаем значение для clothingUrl через рефлексию
+        ReflectionTestUtils.setField(findService, "clothingUrl", "clothing-service:8080");
+    }
+
+    @Test
+    public void getAllProducts_PersonIsNotAunt_BadRequest()
+    {
+        AuntService.auntPerson = null;
+        ResponseEntity<?> response =  findService.getAllProducts();
+
+        assertEquals("Войдите в систему", response.getBody());
+    }
+
+    @Test
+    public void getAllProducts_PersonIsAunt_OkRequest()
+    {
+        AuntService.auntPerson = new Person();
+        when(restTemplate.getForObject(anyString(), eq(String.class))).thenReturn("answer");
+
+        ResponseEntity<?> response =  findService.getAllProducts();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("answer", response.getBody());
+        verify(restTemplate).getForObject(
+                "http://clothing-service:8080/find/all",
+                String.class
+        );
+
+    }
+
 }
--- User/src/test/java/org/example/serviceTest/InfoServiceTest.java
+++ User/src/test/java/org/example/serviceTest/InfoServiceTest.java
@@ -1,4 +1,9 @@
 package org.example.serviceTest;
 
-public class InfoServiceTest {
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class InfoServiceTest
+{
 }
